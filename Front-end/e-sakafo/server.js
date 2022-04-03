7f797d8c4c15c0a3626332681edf55976a228d08
Riantsoa Ramanampamonjy <ramanampamonjysandratra@gmail.com>
2022-04-04 00:21:57 +0300

    mises à jour à déployer sur heroku

--- Front-end/e-sakafo/server.js
+++ Front-end/e-sakafo/server.js
@@ -111,8 +111,6 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
     // ========================
     const isProduction = process.env.NODE_ENV === 'production'
     const port = isProduction ? 7500 : 3000
-    app.listen(port, function () {
-      console.log(`listening on ${port}`)
-    })
+    app.listen(process.env.PORT || 8080);
   })
   .catch(console.error)

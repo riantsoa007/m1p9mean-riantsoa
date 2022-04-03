5f919573f287d3f213585a4f45c68c18a3f72206
Riantsoa Ramanampamonjy <ramanampamonjysandratra@gmail.com>
2022-04-04 00:26:31 +0300

    mises à jour à déployer sur heroku

--- Front-end/e-sakafo/server.js
+++ Front-end/e-sakafo/server.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.static(__dirname + '/dist/e-sakafo'));
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname +
-    '/dist/e-sakafo/index.html'));
+    '/src/index.html'));
 });
 // ========================
 // Link to Database

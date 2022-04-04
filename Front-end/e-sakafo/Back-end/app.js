02079298cca2405ed309b0f3b00df7993640bf27
Riantsoa Ramanampamonjy <ramanampamonjysandratra@gmail.com>
2022-04-05 00:02:38 +0300

    'build'

--- Front-end/e-sakafo/Back-end/app.js
+++ Front-end/e-sakafo/Back-end/app.js
@@ -1,12 +1,11 @@
 
 const express = require('express')
-let app = express();
 const mangose = require('mongoose');
 let cors = require('cors');
 const { default: mongoose } = require('mongoose');
 var userRoute = require('./routes/UserRoute');
 const path = require('path')
-
+const app = express()
 //Gestion Cors
 app.use(cors({
     origin: "http://localhost:3000/"
@@ -22,14 +21,14 @@ app.use((req, res, next) => {
 //conection mangoDB
 const connectionString = process.env.DB_URL
 
-mongoose.connect(connectionString)
+mongoose.connect("mongodb+srv://admin:root@cluster0.rf865.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     .then(() => {
         console.log('Connection etablie')
     }).catch((error) => {
         console.log(error);
     });
 
-app.use('/api/user', userRoute);
+//app.use('/api/user', userRoute);
 
 app.use(express.static('dist/e-sakafo'));
 
---
+++ Front-end/e-sakafo/Back-end/routes/UserRoute.js
@@ -0,0 +1,4 @@
+// const { create, Get } = require("../controllers/UserController");
+
+// router.post('/newUser', create);
+// router.get("/getUser", Get);

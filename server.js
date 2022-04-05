2267dd5a71574e3ead9d0c18e2b1a303127bbe5d
Riantsoa Ramanampamonjy <ramanampamonjysandratra@gmail.com>
2022-04-06 00:47:42 +0300

    'depl'

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-const express = require('express');
-const path = require('path');
+
 let app = require('./Back-end/app');
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/Front-end'));
 
-app.get('/*', (req, res) =>
-  res.sendFile('index.html', { root: 'dist/Front-end/' }),
-);
 // Start the app by listening on the default Heroku port
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 8080;
 
 let server = app.listen(port, function () {
   console.log('Express server listening on port ' + port);
